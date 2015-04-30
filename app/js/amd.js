1dd811f87fe4af9b72476f0a7c744c66eff61db5
Max Waterman <max.waterman@intel.com>
2015-04-30 07:34:20 +0100

    loading amd.js synchronously and removed requireReady promise

--- app/js/amd.js
+++ app/js/amd.js
@@ -1,23 +1,19 @@
-requireReady = new Promise(function (resolve, reject) {
-
-  require.config({
-    paths: {
-      'q': '../../bower_components/q/q',
-      'lodash': '../../bower_components/lodash/lodash',
-      'js-signals': '../../bower_components/js-signals/dist/signals',
-      'React': '../../bower_components/react/react'
-    }
-  });
-
-  // this is here as the r.js optimiser uses the first require.config
-  // call in the file to configure its build (i.e. to minify and concat
-  // all the JS files used by the app via require); the argument passed
-  // to that first call must be valid JSON, and the below config is not
-  var urlArgs = (document.location.href.match(/nocache/) ?
-                 'bust=' + (new Date()).getTime() :
-                 '');
-
-  require.config({urlArgs: urlArgs});
-
-  resolve();
+require.config({
+  baseUrl: './js',
+  paths: {
+    'q': '../../bower_components/q/q',
+    'lodash': '../../bower_components/lodash/lodash',
+    'js-signals': '../../bower_components/js-signals/dist/signals',
+    'React': '../../bower_components/react/react'
+  }
 });
+
+// this is here as the r.js optimiser uses the first require.config
+// call in the file to configure its build (i.e. to minify and concat
+// all the JS files used by the app via require); the argument passed
+// to that first call must be valid JSON, and the below config is not
+var urlArgs = (document.location.href.match(/nocache/) ?
+               'bust=' + (new Date()).getTime() :
+               '');
+
+require.config({urlArgs: urlArgs});
